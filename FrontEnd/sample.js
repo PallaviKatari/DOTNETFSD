362f21fcdbfe1dce21a0ee29cde7639b510060ff
PallaviKatari <pallavikatari@gmail.com>
2022-03-25 10:41:44 +0530

    Commit_Strings

--- FrontEnd/sample.js
+++ FrontEnd/sample.js
@@ -1,4 +1,15 @@
 function message2()
 {
-    alert("I am an External JS File");
+    //alert("I am an External JS File");
+    var age =20;
+var nm="Alex";
+var ab25= true;
+var height= 1.59;
+DA= null;
+document.write("Name: "+nm+"<br>");
+document.write("Age:"+age+"<br>");
+document.write("Below 25:"+ab25+"<br>");
+document.write("Height:"+height+"<br>");
+document.write("Differently abled:"+DA+"<br>");
 }
+
